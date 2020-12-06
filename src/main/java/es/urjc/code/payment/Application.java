7590b3afd7dad053255708274f301652f996e027
François <fpoirier2002@gmail.com>
2020-12-06 16:05:04 +0100

    @EnableDiscoveryClient declaration

--- src/main/java/es/urjc/code/payment/Application.java
+++ src/main/java/es/urjc/code/payment/Application.java
@@ -2,10 +2,12 @@ package es.urjc.code.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("es.urjc.code.payment.infrastructure.adapter.repository.jpa")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     
 	public static void main(String[] args) {
